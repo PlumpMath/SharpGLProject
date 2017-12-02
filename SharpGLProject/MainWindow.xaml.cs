27719e3bbd68e8e40f73760b4636629170fdadb8
Stadzior <Stadzior@gmail.com>
2017-12-02 18:17:56 +0100

    sample figures.

--- SharpGLProject/MainWindow.xaml.cs
+++ SharpGLProject/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SharpGL;
+using SharpGL.SceneGraph;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +25,140 @@ namespace SharpGLProject
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OpenGLControl_OpenGLDraw(object sender, OpenGLEventArgs args)
+        {
+            //  Get the OpenGL instance that's been passed to us.
+            OpenGL gl = args.OpenGL;
+
+            //  Clear the color and depth buffers.
+            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+
+            //  Reset the modelview matrix.
+            gl.LoadIdentity();
+
+            //  Move the geometry into a fairly central position.
+            gl.Translate(-1.5f, 0.0f, -6.0f);
+
+            //  Draw a pyramid. First, rotate the modelview matrix.
+            gl.Rotate(rotatePyramid, 0.0f, 1.0f, 0.0f);
+
+            //  Start drawing triangles.
+            gl.Begin(OpenGL.GL_TRIANGLES);
+
+            gl.Color(1.0f, 0.0f, 0.0f);
+            gl.Vertex(0.0f, 1.0f, 0.0f);
+            gl.Color(0.0f, 1.0f, 0.0f);
+            gl.Vertex(-1.0f, -1.0f, 1.0f);
+            gl.Color(0.0f, 0.0f, 1.0f);
+            gl.Vertex(1.0f, -1.0f, 1.0f);
+
+            gl.Color(1.0f, 0.0f, 0.0f);
+            gl.Vertex(0.0f, 1.0f, 0.0f);
+            gl.Color(0.0f, 0.0f, 1.0f);
+            gl.Vertex(1.0f, -1.0f, 1.0f);
+            gl.Color(0.0f, 1.0f, 0.0f);
+            gl.Vertex(1.0f, -1.0f, -1.0f);
+
+            gl.Color(1.0f, 0.0f, 0.0f);
+            gl.Vertex(0.0f, 1.0f, 0.0f);
+            gl.Color(0.0f, 1.0f, 0.0f);
+            gl.Vertex(1.0f, -1.0f, -1.0f);
+            gl.Color(0.0f, 0.0f, 1.0f);
+            gl.Vertex(-1.0f, -1.0f, -1.0f);
+
+            gl.Color(1.0f, 0.0f, 0.0f);
+            gl.Vertex(0.0f, 1.0f, 0.0f);
+            gl.Color(0.0f, 0.0f, 1.0f);
+            gl.Vertex(-1.0f, -1.0f, -1.0f);
+            gl.Color(0.0f, 1.0f, 0.0f);
+            gl.Vertex(-1.0f, -1.0f, 1.0f);
+
+            gl.End();
+
+            //  Reset the modelview.
+            gl.LoadIdentity();
+
+            //  Move into a more central position.
+            gl.Translate(1.5f, 0.0f, -7.0f);
+
+            //  Rotate the cube.
+            gl.Rotate(rquad, 1.0f, 1.0f, 1.0f);
+
+            //  Provide the cube colors and geometry.
+            gl.Begin(OpenGL.GL_QUADS);
+
+            gl.Color(0.0f, 1.0f, 0.0f);
+            gl.Vertex(1.0f, 1.0f, -1.0f);
+            gl.Vertex(-1.0f, 1.0f, -1.0f);
+            gl.Vertex(-1.0f, 1.0f, 1.0f);
+            gl.Vertex(1.0f, 1.0f, 1.0f);
+
+            gl.Color(1.0f, 0.5f, 0.0f);
+            gl.Vertex(1.0f, -1.0f, 1.0f);
+            gl.Vertex(-1.0f, -1.0f, 1.0f);
+            gl.Vertex(-1.0f, -1.0f, -1.0f);
+            gl.Vertex(1.0f, -1.0f, -1.0f);
+
+            gl.Color(1.0f, 0.0f, 0.0f);
+            gl.Vertex(1.0f, 1.0f, 1.0f);
+            gl.Vertex(-1.0f, 1.0f, 1.0f);
+            gl.Vertex(-1.0f, -1.0f, 1.0f);
+            gl.Vertex(1.0f, -1.0f, 1.0f);
+
+            gl.Color(1.0f, 1.0f, 0.0f);
+            gl.Vertex(1.0f, -1.0f, -1.0f);
+            gl.Vertex(-1.0f, -1.0f, -1.0f);
+            gl.Vertex(-1.0f, 1.0f, -1.0f);
+            gl.Vertex(1.0f, 1.0f, -1.0f);
+
+            gl.Color(0.0f, 0.0f, 1.0f);
+            gl.Vertex(-1.0f, 1.0f, 1.0f);
+            gl.Vertex(-1.0f, 1.0f, -1.0f);
+            gl.Vertex(-1.0f, -1.0f, -1.0f);
+            gl.Vertex(-1.0f, -1.0f, 1.0f);
+
+            gl.Color(1.0f, 0.0f, 1.0f);
+            gl.Vertex(1.0f, 1.0f, -1.0f);
+            gl.Vertex(1.0f, 1.0f, 1.0f);
+            gl.Vertex(1.0f, -1.0f, 1.0f);
+            gl.Vertex(1.0f, -1.0f, -1.0f);
+
+            gl.End();
+
+            //  Flush OpenGL.
+            gl.Flush();
+
+            //  Rotate the geometry a bit.
+            rotatePyramid += 3.0f;
+            rquad -= 3.0f;
+        }
+
+        float rotatePyramid = 0;
+        float rquad = 0;
+
+        private void OpenGLControl_OpenGLInitialized(object sender, OpenGLEventArgs args)
+        {
+            //  Enable the OpenGL depth testing functionality.
+            args.OpenGL.Enable(OpenGL.GL_DEPTH_TEST);
+        }
+
+        private void OpenGLControl_Resized(object sender, OpenGLEventArgs args)
+        {
+            //  Get the OpenGL instance.
+            OpenGL gl = args.OpenGL;
+
+            //  Load and clear the projection matrix.
+            gl.MatrixMode(OpenGL.GL_PROJECTION);
+            gl.LoadIdentity();
+
+            // Calculate The Aspect Ratio Of The Window
+            gl.Perspective(45.0f, (float)gl.RenderContextProvider.Width / (float)gl.RenderContextProvider.Height,
+                0.1f, 100.0f);
+
+            //  Load the modelview.
+            gl.MatrixMode(OpenGL.GL_MODELVIEW);
         }
     }
 }
